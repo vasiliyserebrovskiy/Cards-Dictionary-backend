2360c77439021671732d08296b47e56f61b21d66
SerebrovskiyV <vasiliy.serebrovskiy@gmail.com>
2025-06-22 15:07:15 +0200

    initial commit

---
+++ src/main/java/com/sitool/cardsdictionary/CardsDictionaryAppApplication.java
@@ -0,0 +1,13 @@
+package com.sitool.cardsdictionary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardsDictionaryAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CardsDictionaryAppApplication.class, args);
+    }
+
+}
